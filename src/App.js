1e632c6feb74e93d0fb75c064f9a0e3b16943acd
Sergei Savransky <savran46@mail.ru>
2021-08-13 13:54:30 +0300

    - fix main page styles and wrapper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import styles from "./App.module.css";
 import Footer from "./components/footer/footer";
 import Home from "./components/home/home";
 import TopMenu from "./components/top-menu/top-menu";
@@ -8,11 +9,11 @@ import TopMenu from "./components/top-menu/top-menu";
 function App() {
   return (
     <Router>
-      <div className="App">
+      <main className={styles.main}>
         <Route path={'*'} component={TopMenu}/>
         <Route path={'/'} component={Home}/>
         <Route path={'*'} component={Footer}/>
-      </div>
+      </main>
     </Router>
   );
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,4 +1,3 @@
-import styles from "../../App.css";
 import Header from "../header/header";
 import About from "../about/about";
 import ContactForm from "../contact-form/contact-form";
@@ -8,11 +7,11 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <>
       <Header/>
       <About/>
       <ContactForm/>
       <Contacts/>
-    </main>
+    </>
   )
 }
